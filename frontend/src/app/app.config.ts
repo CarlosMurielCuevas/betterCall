79680ec8c0dede9202b5be2f065192ac3212b637
Carlos <murielcaco2004@gmail.com>
2025-11-16 17:40:45 +0100

    pagina principal

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { Home } from './pages/home/home';
 
 export const appConfig: ApplicationConfig = {
   providers: [
